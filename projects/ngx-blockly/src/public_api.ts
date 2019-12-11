18367319b8790ea6da5ea6d10f4a9af2c9e5e69c
Robin Röttger <robin.roettger@beemo.eu>
2019-12-11 17:16:34 +0100

    Allow nested categories

--- projects/ngx-blockly/src/public_api.ts
+++ projects/ngx-blockly/src/public_api.ts
@@ -3,7 +3,6 @@
  */
 
 export * from './lib/ngx-blockly/ngx-blockly.component';
-export * from './lib/ngx-blockly/ngx-blockly.config';
 export * from './lib/ngx-blockly/ngx-blockly-generator.config';
 export * from './lib/ngx-blockly/models/block';
 export * from './lib/ngx-blockly/models/custom-block';
